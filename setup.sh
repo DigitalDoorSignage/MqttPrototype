41cad6a4930deaeb60c92b83e5eab773504d9bae
Tim Untersberger <timuntersberger2@gmail.com>
2018-11-05 10:22:08 -0800

    Fresh start

--- setup.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/eclipse/paho.mqtt.c
-&& cd paho.mqtt.c
-&& cmake
-&& sudo make install
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include "mqtt/client.h"
+#include <mqtt/client.h>
 
 int main() {
-    mqtt::client client();
-    std::cout << "Hello, World!" << std::endl;
+    mqtt::client client("127.0.0.1:1883", "test-client");
+
+    client.connect();
+    client.disconnect();
     return 0;
 }
+
